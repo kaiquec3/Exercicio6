798d851cce0f8637403d082916ea19f0cdcc360c
kaiquec3 <74798358+kaiquec3@users.noreply.github.com>
2020-11-20 20:15:04 -0300

    Add files via upload

---
+++ Exercício 6/Exercício 6/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercício_6
+{
+    class MilhaMar    
+    {
+        double valor;
+        double convertido;
+
+        public void SetValor(double p)
+        {
+            valor = p;
+        }
+
+        public double GetValor()
+        {
+            return valor;
+        }
+
+        public double GetConvertido()
+        {
+            return convertido;
+        }
+
+        public double ConverterMilhaMarParaMetros()
+        {
+            convertido = valor * 1852;
+            return convertido;
+        }
+
+    }
+}
---
+++ Exercício 6/Exercício 6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercício_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a1 = new MilhaMar();
+
+            Console.Write("Informe o valor da distância em milhas marítmas: ");
+            a1.SetValor(double.Parse(Console.ReadLine()));
+
+            if(a1.GetValor() == 1)
+            {
+                Console.WriteLine($"{a1.GetValor()} milha marítma = {a1.ConverterMilhaMarParaMetros()} metros");
+            }
+            else if(a1.GetConvertido() == 1)
+            {
+                Console.WriteLine($"{a1.GetValor()} milhas marítmas = {a1.ConverterMilhaMarParaMetros()} metro");
+            }
+            else
+            {
+                Console.WriteLine($"{a1.GetValor()} milhas marítmas = {a1.ConverterMilhaMarParaMetros()} metros");
+            }
+        }
+    }
+}
